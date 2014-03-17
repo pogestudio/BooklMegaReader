d0a838c91f6004c369ee101d5b6e5a3e1f2fe611
CA Ewerbring <caewerbring@gmail.com>
2014-03-17 17:25:03 +0100

    First commit

---
+++ BooklEpicReader/BRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRAppDelegate.h
+//  BooklEpicReader
+//
+//  Created by CAwesome on 2014-03-17.
+//  Copyright (c) 2014 CAwesome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BooklEpicReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BooklEpicReader
+//
+//  Created by CAwesome on 2014-03-17.
+//  Copyright (c) 2014 CAwesome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRAppDelegate class]));
+    }
+}
